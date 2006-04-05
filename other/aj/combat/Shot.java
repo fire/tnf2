728dd45e56b454f542d7a605220d345dd276b83c
flandar <flandar>
2006-04-05 14:49:47 +0000

    Changed idtype

--- other/aj/combat/Shot.java
+++ other/aj/combat/Shot.java
@@ -12,7 +12,9 @@ import aj.misc.Stuff;
  *@created    April 12, 2000 
  */
 public class Shot extends Thing implements CombatItem {
-	public static final String shotType = "1000";
+	public static final String shotType = "L";
+
+	long minShotLife=2000;
 	long lifeOver;
 	double mx, my;
 
@@ -29,7 +31,7 @@ public class Shot extends Thing implements CombatItem {
 	 *@param  vy    Description of Parameter 
 	 *@param  life  Description of Parameter 
 	 */
-	public Shot(String id, double x, double y, double d, double vx, double vy, String life) {
+	public Shot(String id, double x, double y, double d, double vx, double vy) {
 		this.id = id;
 		this.x = x;
 		this.y = y;
@@ -38,7 +40,7 @@ public class Shot extends Thing implements CombatItem {
 		this.vx = vx;
 		this.vy = vy;
 		this.time = System.currentTimeMillis();
-		lifeOver = time + Integer.parseInt(life);
+		lifeOver = time + minShotLife;
 		mx = Math.cos(dir) * size / 2;
 		my = Math.sin(dir) * size / 2;
 	}
@@ -60,7 +62,6 @@ public class Shot extends Thing implements CombatItem {
 	 *@param  g  Description of Parameter 
 	 */
 	public void display(Graphics g) {
-		updatePos();
 		g.setColor(Color.white);
 		g.drawLine((int) (x - mx), (int) (y - my), (int) (x + mx), (int) (y + my));
 	}
@@ -72,23 +73,23 @@ public class Shot extends Thing implements CombatItem {
 	 *@return    Description of the Returned Value 
 	 */
 	public String toString() {
-		return id + " " 
-		+ Stuff.trunc(x,1) + " " + 
+		return shotType+" "+
+		id + " "+ 
+		Stuff.trunc(x,1) + " " + 
 		Stuff.trunc(y,1) + " " + 
 		Stuff.trunc(dir,2) + " " + 
-		Stuff.trunc(vx,2) + " " + 
-		Stuff.trunc(vy,2) +" "+ 
-		shotType;
+		Stuff.trunc(vx,3) + " " + 
+		Stuff.trunc(vy,3) +" ";
 	}
 
 
 	public static CombatItem parse(String[] t) {
-		return new Shot(t[0], Double.parseDouble(t[1]), 
-				Double.parseDouble(t[2]), 
-				Double.parseDouble(t[3]), 
-				Double.parseDouble(t[4]), 
-				Double.parseDouble(t[5]), 
-				t[6]);
+		double x=Double.parseDouble(t[2]);
+		double y=Double.parseDouble(t[3]);
+		double dir=Double.parseDouble(t[4]);
+		double vx=Double.parseDouble(t[5]);
+		double vy=Double.parseDouble(t[6]);
+		return new Shot(t[1], x,y,dir,vx,vy);
 	}
 
 }
